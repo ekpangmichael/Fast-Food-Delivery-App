77797d852c33643cb171459de9381a81ce2ee034
Ekpang Michael <weezyval93@gmail.com>
2018-09-05 18:00:50 -0700

    Added More Test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,54 +1,111 @@
 import request from 'supertest';
-import httpStatus from 'http-status';
+import chaiHttp from 'chai-http';
 import chai, { expect } from 'chai';
 import { app, server } from '../server';
 
-describe('## API Tests', () => {
+chai.use(chaiHttp);
 
-  after( (done) => {
-        server.close();
-        done();
-    });
+const orders = {
+  userId: '2',
+  orderId: '7',
+  orderName: 'beans',
+  quantity: '200',
+  price: 'N200',
+  imgUrl: 'jjjssj.jpg',
+};
+
+const orders2 = {
+  userId: '2',
+  orderId: '7',
+  orderName: 'beans',
+};
+
+describe('## API Tests', () => {
+  after((done) => {
+    server.close();
+    done();
+  });
+
   describe('### POST /Orders', () => {
-    
     it('should create a user and get back a response', (done) => {
-      const orders = {
-        userId: '2',
-        orderId: '7',
-        orderName: 'beans',
-        quantity: '200',
-        price: 'N200',
-        imgUrl: 'jjjssj.jpg',
-      };
 
       request(app)
         .post('/api/v1/orders')
         .send(orders)
         .end((err, res) => {
           const data = JSON.parse(res.text);
+          const order = data[1].orders;
           const name = data[1].orders.orderName;
+          expect(err).to.be.a('null');
           expect(name).to.equal(orders.orderName);
+          expect(data[0].message).to.equal('Item added successfully');
           done();
         });
     });
-    // it('should return the created order successfully', (done) => {
-    //   request(app)
-    //     .post('/api/v1/orders')
-    //     .send({
-    //       userId: '2',
-    //       orderId: '7',
-    //       orderName: 'beans',
-    //       quantity: '200',
-    //       price: 'N200',
-    //       imgUrl: 'jjjssj.jpg',
-    //     })
-    //     .expect(httpStatus.Ok)
-    //     .then((res) => {
-    //       expect(res.message).to.equal('Item added successfully');
-    //       //expect(res.body.orderName).to.equal('beans');
-    //       //expect(res.body.orderId).to.exist();
-    //       done();
-    //     });
-    // });
+
+    it('should return  bad request error', (done) => {
+      request(app)
+        .post('/api/v1/orders')
+        .send(orders2)
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          const data = JSON.parse(res.text);
+          expect(data.message).to.equal('All fields are required');
+          done();
+        });
+    });
+
+    it('Order should return an object', (done) => {
+      request(app)
+        .get('/api/v1/orders')
+        .send(orders)
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          const data = JSON.parse(res.text);
+          const dataObject = data[1].orders[0];
+          //console.log(dataObject);
+          expect(Object.keys(dataObject)).to.have.lengthOf(10);
+          expect(data[0].message).to.equal('successful');
+          done();
+        });
+    });
+
+    it('Order should return 404 error', (done) => {
+      request(app)
+        .get('/api/v1/orders/:id')
+        .send()
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+          const data = JSON.parse(res.text);
+          expect(data.message).to.equal('Order not found');
+          done();
+        });
+    });
+
+
+    it('Order should return 404 error - Order not found ', (done) => {
+      request(app)
+        .put('/api/v1/orders/:id')
+        .send()
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+          const data = JSON.parse(res.text);
+          expect(data.message).to.equal('Order not found');
+          done();
+        });
+    });
+
+    it('Order should return 404 error - Order not found ', (done) => {
+      request(app)
+        .delete('/api/v1/orders/:id')
+        .send()
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+          const data = JSON.parse(res.text);
+          expect(data.message).to.equal('Order not found');
+          done();
+        });
+    });
+
   });
 });
