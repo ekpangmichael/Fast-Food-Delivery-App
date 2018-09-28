fe7af84b8cad399ec838fb2bafb4187b3386a43c
Ekpang Michael <weezyval93@gmail.com>
2018-09-28 12:00:06 +0100

    feat (users sigin, signup, authentication): api users route
    - implement user sign and signup with persistent data
    - create user table
    - update user controller
    - update package.json file
    - update server file
    - update user route file
    
    [Delivers #160839981]

--- server/api/controllers/orders.js
+++ server/api/controllers/orders.js
@@ -24,24 +24,24 @@ const Orders = {
     try {
       const { rows, rowCount } = await db.query(findAllQuery);
       return res.status(200).send({ rows, rowCount });
-    } catch(error) {
+    } catch (error) {
       return res.status(400).send(error);
     }
   },
-  
+
   async getOne(req, res) {
     const text = 'SELECT * FROM orders WHERE id = $1';
     try {
       const { rows } = await db.query(text, [req.params.id]);
       if (!rows[0]) {
-        return res.status(404).send({'message': 'orders not found'});
+        return res.status(404).send({ message: 'orders not found' });
       }
       return res.status(200).send(rows[0]);
-    } catch(error) {
-      return res.status(400).send(error)
+    } catch (error) {
+      return res.status(400).send(error);
     }
   },
-}
+};
 
 
 export default Orders;
