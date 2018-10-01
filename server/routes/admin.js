8831a4df4814944449ffa5f77934b9dd92feda9e
Ekpang Michael <weezyval93@gmail.com>
2018-10-01 18:24:40 +0100

    bug (server.js): re-arrange folder structure - modify users route - modify admin route - modify orders route [Fixes #160900267]

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import Admin from '../controllers/Admin';
+import Admin from '../controllers/admin';
 
 // Express router
 const router = express.Router();
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import Users from '../controllers/Users';
+import Users from '../controllers/users';
 import Auth from '../middleware/Auth';
 
 // Express router
